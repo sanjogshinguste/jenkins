8490fc3d416ab1286f1e5d1bc4243582abb4788e
sanjog <sanjushinguste@gmail.com>
2024-05-30 22:42:55 +0530

    jenkins
    Signed-off-by: sanjog <sanjushinguste@gmail.com>

---
+++ src/test/java/com/example/SpringSecutiy/SpringSecutiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecutiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutiyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
